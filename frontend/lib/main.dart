a1c52b054db943d4153327e0abd334a8dc7d6d34
Jihyun <jose1123@naver.com>
2026-03-25 18:23:12 +0900

    Revert "fix(auth): Firebase/FCM 초기화 코드 제거 (미구현 기능)"
    This reverts commit 6bc2f3e5128196c70b48092f3e6beb06e61b8eb1.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,5 @@
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
@@ -5,8 +7,9 @@ import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   KakaoSdk.init(
     nativeAppKey: const String.fromEnvironment('KAKAO_NATIVE_KEY'),
   );
@@ -15,6 +18,7 @@ void main() {
 
 class FlorentApp extends StatelessWidget {
   const FlorentApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
